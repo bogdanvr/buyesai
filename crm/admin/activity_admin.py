2e6be92b22cc7d4a45139b0ec08951f1e91c0262
bogdanvr <bgdnv2211@gmail.com>
2026-03-16 14:28:12 +0600

    feat add hide fields for activity, deal and lead add company to activity_admin

--- crm/admin/activity_admin.py
+++ crm/admin/activity_admin.py
@@ -5,7 +5,7 @@ from crm.models import Activity
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ("id", "type", "subject", "is_done", "due_at", "created_at")
+    list_display = ("id", "type", "subject", "client", "is_done", "due_at", "created_at")
     list_filter = ("type", "is_done")
     search_fields = ("subject", "description")
     autocomplete_fields = ("lead", "deal", "client", "contact", "created_by")
