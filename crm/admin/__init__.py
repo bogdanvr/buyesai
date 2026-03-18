b32d92e4f1e2dc5de5ef072499e1ba233b3efd4f
bogdanvr <bgdnv2211@gmail.com>
2026-03-18 21:26:03 +0600

    fix admin

--- crm/admin/__init__.py
+++ crm/admin/__init__.py
@@ -2,3 +2,4 @@ from crm.admin.activity_admin import *  # noqa: F401,F403
 from crm.admin.client_admin import *  # noqa: F401,F403
 from crm.admin.deal_admin import *  # noqa: F401,F403
 from crm.admin.lead_admin import *  # noqa: F401,F403
+from crm.admin.touch_admin import *  # noqa: F401,F403
